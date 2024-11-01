6b27a5a72e072ec418dbd6b4ab61a58da02a22bf
Bxtohur <bitohur@gmail.com>
2024-11-01 22:06:12 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return <div>
+    <section className="h-screen bg-green-400 items-center justify-center m-12 flex text-center text-3xl md:p-16 lg:p-16 xl:p-20">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ipsa ipsum dolorum modi laudantium dolorem ut nihil exercitationem eveniet quod illum vitae, itaque, eum atque totam culpa cupiditate, debitis ipsam.</p>
+    </section>
+  </div>;
+};
+
+export default App;
